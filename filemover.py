73898a10cf45ed899e0643878065d42a7fc469e1
tshihad <tshihad9@gmail.com>
2018-12-12 00:38:47 +0530

    initial commit

---
+++ filemover.py
@@ -0,0 +1,105 @@
+import subprocess
+import toml
+import os
+import shutil
+import time
+import logging
+
+LOG_FILE_NAME = 'mover.log'
+SOURCE = 'source'
+DESTINATION = 'destination'
+FILE_NAME = 'filename'
+CREDENTIALS = 'credentials.toml'
+CONFIG = 'config.toml'
+NETWORK_PATH ='networkPath'
+
+def validate(config):
+    if os.path.isdir(config[SOURCE]) == False:
+        logging.error("Source folder doest exists")
+        print("Source folder doest exists")
+        return False
+
+    if os.path.isdir(config[DESTINATION]) == False:
+        print("Destination folder doest exists")
+        logging.error("Destination folder doest exists")
+        return False
+    return True
+
+def read_config(filename):
+    if os.path.isfile(filename) == False:
+        print("config file " + filename + " doest exists")
+        logging.error("config file " + filename + " doest exists")
+        os._exit(2)
+    configfile = os.path.expanduser(filename)
+
+    if os.path.isfile(configfile):
+        with open(configfile, 'r') as f:
+            config = toml.loads(f.read())
+            return config
+
+
+def connect(config):
+    networkPath = config[NETWORK_PATH]
+    user = config['user']
+    password = config['password']
+    winCMD = 'NET USE ' + networkPath + ' /User:' + user + ' ' + password
+    try:
+        subprocess.Popen(winCMD, stdout=subprocess.PIPE, shell=True)
+        print(subprocess.check_output(winCMD))
+        logging.info(subprocess.check_output(winCMD))
+    except Exception as ex:
+        logging.error(ex)
+        print(ex)
+
+
+def mover(config):
+    listOfFiles = getListOfFiles(config)
+
+    if len(listOfFiles) == 0:
+        return
+    src = config[SOURCE]
+    dest = config[DESTINATION]
+
+# only moves first file
+    try:
+        print("moving file " + src+'\\'+listOfFiles[0] + " to " +dest+'\\'+config[FILE_NAME])
+        logging.info("moving file " + src+'\\'+listOfFiles[0] + " to " +dest+'\\'+config[FILE_NAME])
+        shutil.move(src+'\\'+listOfFiles[0],dest+'\\'+config[FILE_NAME])
+    except Exception as ex:
+        logging.info(ex)
+        print(ex)
+
+
+def getListOfFiles(config):
+    return os.listdir(config[SOURCE])
+
+def setLogger():
+    # logging.basicConfig(filename = LOG_FILE_NAME , level = logging.INFO)
+    logging.basicConfig(filename = LOG_FILE_NAME, level = logging.INFO,\
+    format="%(asctime)s - %(name)s - %(message)s")
+
+if __name__ == "__main__":
+    setLogger()
+    print( "initializing program")
+    logging.info( "initializing program")
+    credentials = read_config(CREDENTIALS)
+    config = read_config(CONFIG)
+    
+    print("connecting to remote " + credentials[NETWORK_PATH]) 
+    logging.info("connecting to remote " + credentials[NETWORK_PATH]) 
+    connect(credentials)
+
+    print("validating" )
+    logging.info("validating" )
+    if validate(config) == False:
+        print("program exiting")
+        logging.info("program exiting")
+
+    print('waiting for files')
+    logging.info('waiting for files')
+    while True:
+        mover(config)
+        time.sleep(10)
+
+    raw_input('press enter button to exit\n')
+
